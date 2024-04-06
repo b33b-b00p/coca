757756d199fc2e3f433321730209bb0c4e42be6e
b33b-b00p <103651421+b33b-b00p@users.noreply.github.com>
2024-04-06 17:22:37 +0400

    added insight section

--- src/js/main.js
+++ src/js/main.js
@@ -3,14 +3,9 @@ import '../assets/scss/main.scss';
 //components
 import { changeTheme } from './components/theme.js';
 changeTheme();
-// const body = document.querySelector('.body');
-// const themeSwitch = document.querySelector('.header__switcher');
 
-// themeSwitch.addEventListener('change', () => {
-//     console.log('hello');
-//     body.getAttribute('data-theme') === 'light'
-//         ? body.setAttribute('data-theme', 'dark')
-//         : body.setAttribute('data-theme', 'light');
-// });
 import { useBurger } from './components/burger.js';
 useBurger();
+
+import { useInsightSlider } from './components/slider.js';
+useInsightSlider();

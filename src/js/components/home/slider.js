c7c9d1516b7dd555f562e2f688a2c8e61697bbf6
b33b-b00p <103651421+b33b-b00p@users.noreply.github.com>
2024-04-11 22:30:30 +0400

    added fade effect on slider in testimonials

--- src/js/components/home/slider.js
+++ src/js/components/home/slider.js
@@ -1,8 +1,9 @@
 import Swiper from 'swiper';
-import { Navigation, Scrollbar } from 'swiper/modules';
+import { Navigation, Scrollbar, EffectFade } from 'swiper/modules';
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/scrollbar';
+import 'swiper/css/effect-fade';
 
 export const useInsightSlider = () => {
     new Swiper('.insight__slider', {
@@ -26,7 +27,9 @@ export const useInsightSlider = () => {
 
 export const useTestimonialsSlider = () => {
     new Swiper('.testimonials__slider', {
-        modules: [Navigation],
+        modules: [Navigation, EffectFade],
+        effect: 'fade',
+        crossFade: true,
         slidesPerView: 1,
         simulateTouch: true,
         allowTouchMove: true,

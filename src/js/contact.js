729e3fcf2d44b024b27e6ac86315e7c562628449
b33b-b00p <103651421+b33b-b00p@users.noreply.github.com>
2024-04-18 14:10:30 +0400

    added partners for Contact

--- src/js/contact.js
+++ src/js/contact.js
@@ -4,6 +4,9 @@ import '../assets/scss/contact.scss';
 import { changeTheme } from './components/theme.js';
 changeTheme();
 
+import { usePartnersSlider } from './components/partners.js';
+usePartnersSlider();
+
 import { phoneMask } from './components/contact/phone.js';
 phoneMask();
 
